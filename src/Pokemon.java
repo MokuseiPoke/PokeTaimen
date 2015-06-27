2a67e0971f2a724a074ae627e90193e265d4eef6
DenjiMk2 <13j5137@stu.meisei-u.ac.jp>
2015-06-27 23:28:03 +0900

    新規作成

---
+++ src/Pokemon.java
@@ -0,0 +1,28 @@
+public class Pokemon {
+	enum Type{
+		BUG,
+		DARK,
+		DRAGON,
+		ELECTRIC,
+		FIGHTING,
+		FIRE,
+		FLYING,
+		GHOST,
+		GRASS,
+		GROUND,
+		ICE,
+		NORMAL,
+		ROCK,
+		POISON,
+		PSYCHIC,
+		STEEL,
+		WATER,
+		FAIRY,
+	}
+    private String name;
+    private Type type1;
+    private Type type2;
+    private int ID;
+    private boolean Player;
+
+}
