27481c4d8b0c5d64a40ee4655f43d5faf2c1ba5d
nes <halbin801@gmail.com>
2015-06-28 02:00:27 +0900

    test

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,23 +1,23 @@
 public class Pokemon {
 	enum Type{
-		BUG,
-		DARK,
-		DRAGON,
-		ELECTRIC,
-		FIGHTING,
-		FIRE,
-		FLYING,
-		GHOST,
-		GRASS,
-		GROUND,
-		ICE,
-		NORMAL,
-		ROCK,
-		POISON,
-		PSYCHIC,
-		STEEL,
-		WATER,
-		FAIRY,
+		BUG,		//虫
+		DARK,		//悪
+		DRAGON,		//ドラゴン
+		ELECTRIC,	//電気
+		FIGHTING,	//格闘
+		FIRE,		//炎
+		FLYING,		//飛行
+		GHOST,		//ゴースト
+		GRASS,		//草
+		GROUND,		//地面
+		ICE,		//氷
+		NORMAL,		//ノーマル
+		ROCK,		//岩
+		POISON,		//毒
+		PSYCHIC,	//エスパー
+		STEEL,		//鋼
+		WATER,		//水
+		FAIRY,		//フェアリー
 	}
     private String name;
     private Type type1;
