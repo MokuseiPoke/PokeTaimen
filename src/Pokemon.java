cecb4c42fe02c3a47a20eaf4ebecf7d313282453
DenjiMk2 <13j5137@stu.meisei-u.ac.jp>
2015-07-03 16:40:06 +0900

    Pokemon.javaファイル
    ゲッターセッターコンストラクタを設定

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,28 +1,82 @@
 public class Pokemon {
-	enum Type{
-		BUG,		//虫
-		DARK,		//悪
-		DRAGON,		//ドラゴン
-		ELECTRIC,	//電気
-		FIGHTING,	//格闘
-		FIRE,		//炎
-		FLYING,		//飛行
-		GHOST,		//ゴースト
-		GRASS,		//草
-		GROUND,		//地面
-		ICE,		//氷
-		NORMAL,		//ノーマル
-		ROCK,		//岩
-		POISON,		//毒
-		PSYCHIC,	//エスパー
-		STEEL,		//鋼
-		WATER,		//水
-		FAIRY,		//フェアリー
+	enum Type {
+		BUG, // 虫
+		DARK, // 悪
+		DRAGON, // ドラゴン
+		ELECTRIC, // 電気
+		FIGHTING, // 格闘
+		FIRE, // 炎
+		FLYING, // 飛行
+		GHOST, // ゴースト
+		GRASS, // 草
+		GROUND, // 地面
+		ICE, // 氷
+		NORMAL, // ノーマル
+		ROCK, // 岩
+		POISON, // 毒
+		PSYCHIC, // エスパー
+		STEEL, // 鋼
+		WATER, // 水
+		FAIRY, // フェアリー
+		NONE,	//無し
 	}
-    private String name;
-    private Type type1;
-    private Type type2;
-    private int ID;
-    private boolean Player;
+
+	private String name; // このポケモンの名前
+	private int no; // このポケモンの図鑑No
+	private Type type1; // このポケモンのタイプ１
+	private Type type2; // このポケモンのタイプ２
+	private int id; // このポケモンのGUI上の識別番号
+	private boolean player; // このポケモンはどっちのプレイヤーが持っているか 
+							// true=使用者のポケモン
+							// false=対戦相手のポケモン
+
+	public Pokemon(int id,boolean player,String name){
+		this.id=id;
+		this.player = player;
+		this.name = name;
+	}
+	
+	public Pokemon(int id,boolean player){
+		this(id,player,null);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isPlayer() {
+		return player;
+	}
+
+	public void setPlayer(boolean player) {
+		this.player = player;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+	
+	public void setType(Type type1,Type type2){
+		this.type1 = type1;
+		this.type2 = type2;
+	}
+	
+	public void setType(Type type){
+		this.type1 = type;
+		this.type2 = Type.NONE;
+	}
+	
+	public Type[] getType(){
+		Type type[] = {type1,type2};
+		return type;
+	}
 
 }
